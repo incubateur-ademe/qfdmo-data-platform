d9dad68443a3840c36579bcaff7a248d97a5aa11
Hamza A <azelmat.hamza@gmail.com>
2024-02-22 14:52:00 +0100

    update script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Initialize or upgrade the Airflow database before starting up
-airflow db migrate
 
+
+
+# ln -s `pwd`/dags $AIRFLOW_HOME/dags
 
 airflow scheduler &
 
-# If using CeleryExecutor, also start the Airflow worker in the background
-if [ "$AIRFLOW_EXECUTOR" == "CeleryExecutor" ]; then
-  airflow celery worker &
-fi
-
-airflow webserver --port 8080
-
-# Set the Airflow home environment variable
-#ENV AIRFLOW_HOME=/app
-
-# Expose the Airflow web server port
-EXPOSE 8080
-
-# Start the Airflow web server and scheduler
-#CMD ["airflow", "webserver", "--port", "8080", "&", "airflow", "scheduler"]
-CMD ["airflow","webserver","--port","8080"]
+# start the web server, default port is 8080
+echo "Starting the webserver..."
+port=${PORT:-80}
+airflow webserver --port $port --debug
